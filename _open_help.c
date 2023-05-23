670d30c390c9f90194c7faf4cd577abf6585a6a6
method56 <adjeiakporh56@gmail.com>
2023-05-23 09:45:51 -0700

    fixed gcc errors

--- _open_help.c
+++ _open_help.c
@@ -1,11 +1,11 @@
 #include "simshell.h"
 
 /**
- * _help - Opens the Shell help
+ * _open_help - Opens the Shell help
  *
  * Return: Nothing
  */
-void _help(void)
+void _open_help(void)
 {
 	_puts("\n*** WELCOME TO THE SHELL HELP ***\n");
 }
